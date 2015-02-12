4984ec930e41a0addc0f61e632e952d48ad82f79
nlarusstone <nlarusstone@college.harvard.edu>
2015-02-12 00:44:32 -0500

    Pulling from Goldberg

--- practical1.py
+++ practical1.py
@@ -5,7 +5,7 @@ import numpy as np
 
 train_filename = '../train.csv.gz'
 test_filename  = '../test.csv.gz'
-pred_filename  = 'example_mean.csv'
+pred_filename  = 'practical1.csv'
 
 # Load the training file.
 train_data = []
@@ -33,8 +33,8 @@ with gzip.open(train_filename, 'r') as train_fh:
         counter += 1
 
 # Compute the mean of the gaps in the training data.
-w = np.linalg.solve(np.dot(Phi.T, Phi) , np.dot(Phi.T, Y))
-gaps = np.array([datum['gap'] for datum in train_data])
+w = np.linalg.solve(np.dot(features.T, features) , np.dot(features.T, gap))
+#gaps = np.array([datum['gap'] for datum in train_data])
 #mean_gap = np.mean(gaps)
 
 # Load the test file.
@@ -54,10 +54,12 @@ with gzip.open(test_filename, 'r') as test_fh:
         id       = row[0]
         smiles   = row[1]
         features = np.array([float(x) for x in row[2:258]])
+        gap = np.dot(w.T, features)
         
         test_data.append({ 'id':       id,
                            'smiles':   smiles,
-                           'features': features })
+                           'features': features,
+                            'gap':     gap})
         counter += 1
 
 # Write a prediction file.
@@ -70,4 +72,4 @@ with open(pred_filename, 'w') as pred_fh:
     pred_csv.writerow(['Id', 'Prediction'])
 
     for datum in test_data:
-        pred_csv.writerow([datum['id'], mean_gap])
+        pred_csv.writerow([datum['id'], datum['gap']])
